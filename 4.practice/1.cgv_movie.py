943fb4b84c3e88a818d3b9a779ee3672c1a056c9
dahee325 <sally8323@gmail.com>
2025-04-15 16:10:35 +0900

    cgv movie

--- 4.practice/1.cgv_movie.py
+++ 4.practice/1.cgv_movie.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import csv
 
 driver = webdriver.Chrome()
 
@@ -11,7 +12,7 @@ driver.get(URL)
 
 movie_list = []
 
-for i in range(5):
+for i in range(10):
     time.sleep(2)
     movie_info = driver.find_elements(By.CSS_SELECTOR, 'div.box-contents > a')
     # print(movie_info[i].get_attribute('innerHTML'))
@@ -22,11 +23,30 @@ for i in range(5):
     # print(title)
     director = driver.find_element(By.CSS_SELECTOR, 'div.spec > dl > dd:nth-child(2)').text
     # print(director)
+    
+    actor_list = []
     actors = driver.find_elements(By.CSS_SELECTOR, 'div.spec > dl > dd.on a')
     for actor in actors:
         actor.text
+        actor_list.append(actor.text)
         # print(actor.text)
+    
+    genre = driver.find_element(By.CSS_SELECTOR, 'div.spec dt:nth-of-type(3)').text
+    # print(genre)
+    opening_date = driver.find_element(By.CSS_SELECTOR, 'div.spec dd:nth-of-type(6)').text
+    # print(opening_date)
 
-    movie_list.append([title, director])
+    movie_list.append([title, director, actor_list, genre, opening_date])
 
     driver.back()
+
+# print(movie_list)
+
+local_file_path = '/home/ubuntu/damf2/data/cgv_movie/'
+
+def save_to_csv(movie_list):
+    with open(local_file_path + 'cgv-top-10', 'w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(movie_list)
+
+save_to_csv(movie_list)
